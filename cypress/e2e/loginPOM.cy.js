4279f2d8400d8d768bc2cd6937972e98028fc62e
ELsys-Rondon <elsysrondon01@gmail.com>
2026-03-18 10:48:18 -0300

    Fix CI final

--- cypress/e2e/loginPOM.cy.js
+++ cypress/e2e/loginPOM.cy.js
@@ -1,19 +1,18 @@
-import LoginPage from "../pageObjects/loginPage";
+import LoginPage from "../pageObjects/loginPage"
 
 describe("Login SauceDemo con POM", () => {
 
   it("Login exitoso usando POM y fixture", () => {
-    const login = new LoginPage();
-    
-    cy.fixture("example").then((data) => {
-      login.visit();
-      login.fillUsername(data.standardUser.user);
-      login.fillPassword(data.standardUser.pass);
-      login.submit();
-    });
 
-    cy.url().should("include", "inventory");
-    cy.get(".inventory_list").should("be.visible");
-  });
+    const login = new LoginPage()
 
-});
+    cy.fixture("user").then((user) => {
+      login.visit()
+      login.login(user.username, user.password)  // usa la función login de la clase
+    })
+
+    cy.url().should("include", "inventory")       // verifica que llegó a la página
+    cy.get(".title").should("contain", "Products")
+  })
+
+})
--- cypress/pageObjects/loginPage.js
+++ cypress/pageObjects/loginPage.js
@@ -5,9 +5,9 @@ class LoginPage {
   }
 
   login(username, password) {
-    cy.get("#user-name").type(username)
-    cy.get("#password").type(password)
-    cy.get("#login-button").click()
+    cy.get("#user-name").type(username)   // ingresa el usuario
+    cy.get("#password").type(password)    // ingresa la contraseña
+    cy.get("#login-button").click()       // hace clic en login
   }
 
 }
